fc1026b7bccc38abd1883ecc6802ca50c9a5744e
aysebingol <aysebngll0@gmail.com>
2022-11-05 20:42:57 +0300

    threading

---
+++ repositoryDemo/src/repositoryDemo/ProductDal.java
@@ -0,0 +1,17 @@
+package repositoryDemo;
+
+public class ProductDal implements IEntityRepository<Product> {
+
+	@Override
+	public void add(Product entity) {		
+	}
+
+	@Override
+	public void delete(Product entity) {		
+	}
+
+	@Override
+	public void update(Product entity) {		
+	}
+
+}
